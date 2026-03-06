0244014782c4d09c39418f07403d46a3e6f39071
Anna Vasilevich <dzichonka@gmail.com>
2026-03-06 18:19:55 +0100

    feat: implement fs:restore

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,7 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = "./";
 
 const snapshot = async () => {
   try {
