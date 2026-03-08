292baf5d4b09412de2be6a08b1f4f84a09338fe8
Anna Vasilevich <dzichonka@gmail.com>
2026-03-08 20:01:26 +0100

    feat: implement Threads Worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,9 +1,15 @@
-import { parentPort } from 'worker_threads';
+import { parentPort } from "worker_threads";
 
-// Receive array from main thread
-// Sort in ascending order
-// Send back to main thread
+parentPort.on("message", (data) => {
+  try {
+    if (!Array.isArray(data)) {
+      throw new Error("Received data is not an array");
+    }
 
-parentPort.on('message', (data) => {
-  // Write your code here
+    const sortedArray = data.sort((a, b) => a - b);
+
+    parentPort.postMessage(sortedArray);
+  } catch (error) {
+    parentPort.postMessage({ error: error.message });
+  }
 });
