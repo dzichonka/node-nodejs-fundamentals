87f2059778575934f17d89e231467e23743d0e1e
Anna Vasilevich <dzichonka@gmail.com>
2026-03-08 20:53:33 +0100

    feat: implement child rocessees

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,45 @@
+import process from "node:process";
+import { spawn } from "node:child_process";
+
 const execCommand = () => {
-  // Write your code here
-  // Take command from CLI argument
-  // Spawn child process
-  // Pipe child stdout/stderr to parent stdout/stderr
-  // Pass environment variables
-  // Exit with same code as child
+  const commandString = process.argv[2];
+
+  if (!commandString) {
+    process.exit(1);
+  }
+
+  const parts = commandString.split(" ");
+
+  const command = parts[0];
+
+  const args = parts.slice(1);
+
+  const childProcess = spawn(command, args, {
+    stdio: ["pipe", "pipe", "pipe"],
+    env: { ...process.env },
+    shell: false,
+    cwd: process.cwd(),
+  });
+
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.stderr.pipe(process.stderr);
+
+  childProcess.on("exit", (code) => {
+    process.exit(code || 0);
+  });
+
+  childProcess.on("error", (error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
+
+  process.on("SIGINT", () => {
+    childProcess.kill("SIGINT");
+  });
+  process.on("SIGTERM", () => {
+    childProcess.kill("SIGTERM");
+  });
 };
 
 execCommand();
